7ca54a35f81ed2e48c9f786f490af2092cddf469
Benjamin Barillot <droken.horisk@gmail.com>
2023-08-21 16:39:35 +0200

    feat(BookingsController): add bookingcontroller
    added booking controller and route controller%

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,26 @@
+class BookingsController < ApplicationController
+  def index
+    @bookings = Booking.all
+  end
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
+    @booking.car = Car.find(params[:car_id])
+    if @booking.save
+      redirect_to bookings_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date, :car_id, :user_id)
+  end
+end
