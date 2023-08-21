f28a73832597e02a0332c70039acd5bec17da789
Josue De Almeida Rodrigues <jo.alm.rod@gmail.com>
2023-08-21 21:08:51 +0200

    feat(Navbar): Add navbar with logical connexion
    NavBar model of Le Wagon used for add an logical login/logout%

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  
-  root to: "cars#index"
+
+  # root to: "cars#index"
   resources :cars do
     resources :bookings, only: [:new, :create]
   end
