17cad647e5eadf8b21fa7fa52b5ec08caf5f4b6a
Josue De Almeida Rodrigues <jo.alm.rod@gmail.com>
2023-08-21 16:28:21 +0200

    feat(Controller):Add car controller
    Adding car controller and action%

---
+++ app/controllers/cars_controller.rb
@@ -0,0 +1,60 @@
+class CarsController < ApplicationController
+  before_action :set_car, only: %i[ show edit update destroy ]
+
+  # GET /cars
+  def index
+    @cars = Car.all
+  end
+
+  # GET /cars/1
+  def show
+    @car = Car.new
+  end
+
+  # GET /cars/new
+  def new
+    @car = Car.new
+  end
+
+  # GET /carqs/1/edit
+  def edit
+  end
+
+  # POST /cars
+  def create
+    @car = Car.new(car_param)
+
+    if @garden.save
+      redirect_to @car, notice: "Garden was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /cars/1
+  def update
+    if @garden.update(garden_params)
+      redirect_to @car, notice: "Car was successfully updated.", status: :see_other
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /gardens/1
+  def destroy
+    @car.destroy
+    redirect_to car_url, notice: "Car was successfully destroyed.", status: :see_other
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_car
+      @car = Car.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def car_param
+      params.require(:car).permit(:brand, :model, :price_per_day, :user_id)
+    end
+
+end
